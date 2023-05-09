8199a3076b3f89b7a1455648819b240b782d7584
1Molot <sinkevich1990@gmail.com>
2023-05-09 22:39:28 +0300

    fix:DopSR2NedSocSet

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {NavBar} from "./components/Nav/NavBar";
+import {Profile} from "./components/Profile/Profile";
+import {BrowserRouter, Route} from "react-router-dom";
+import {News} from "./components/News/News";
+import {Music} from "./components/Music/Music";
+import {Settings} from "./components/Settings/Settings";
+import {Header} from "./components/Header/Header";
+import {DialogsContainer} from "./components/Dialogs/DialogsContainer";
+import {UsersContainer} from "./components/User/UsersContainer";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // console.log('props.', props.store.getState())
+    return (
+        <BrowserRouter>
+
+            <div className="App">
+                <Header/>
+                <div className={"navAndContent"}>
+                    <NavBar/>
+                    <div className={'content'}>
+                        <Route
+                            path={"/dialogs"}
+                            render={() => <DialogsContainer/>}/>
+                        <Route path={"/profile"} render={() => <Profile/>}/>
+                        <Route path={"/news"} render={() => <News/>}/>
+                        <Route path={"/music"} render={() => <Music/>}/>
+                        <Route path={"/settings"} render={() => <Settings/>}/>
+                        <Route path={"/users"} render={() => <UsersContainer/>}/>
+                    </div>
+                </div>
+            </div>
+
+        </BrowserRouter>);
 }
 
 export default App;
